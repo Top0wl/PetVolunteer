198ea1f0978dcf8bef6a378757e705e28dcc550d
Top0wl <semen_vervein@mail.ru>
2024-10-13 15:47:14 +0700

    миграции

---
+++ backend/src/PetVolunteer.Infrastructure/Migrations/20241002071831_Mig6.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetVolunteer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mig6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "breed",
+                table: "pets");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "social_media",
+                table: "volunteers",
+                type: "jsonb",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "jsonb");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "requisites",
+                table: "volunteers",
+                type: "jsonb",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "jsonb");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "social_media",
+                table: "volunteers",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "jsonb",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "requisites",
+                table: "volunteers",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "jsonb",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "breed",
+                table: "pets",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ backend/src/PetVolunteer.Infrastructure/Migrations/20241013084011_mig7.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetVolunteer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_pets_volunteers_volunteer_id",
+                table: "pets");
+
+            migrationBuilder.RenameColumn(
+                name: "volunteer_id",
+                table: "pets",
+                newName: "pet_id");
+
+            migrationBuilder.RenameIndex(
+                name: "ix_pets_volunteer_id",
+                table: "pets",
+                newName: "ix_pets_pet_id");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "volunteers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "pets",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_pets_volunteers_pet_id",
+                table: "pets",
+                column: "pet_id",
+                principalTable: "volunteers",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_pets_volunteers_pet_id",
+                table: "pets");
+
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "volunteers");
+
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "pets");
+
+            migrationBuilder.RenameColumn(
+                name: "pet_id",
+                table: "pets",
+                newName: "volunteer_id");
+
+            migrationBuilder.RenameIndex(
+                name: "ix_pets_pet_id",
+                table: "pets",
+                newName: "ix_pets_volunteer_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_pets_volunteers_volunteer_id",
+                table: "pets",
+                column: "volunteer_id",
+                principalTable: "volunteers",
+                principalColumn: "id");
+        }
+    }
+}
